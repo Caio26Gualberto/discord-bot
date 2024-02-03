5e402e77ae8930c5cb98c4e827ccd9691f30356c
Caio26Gualberto <caiogualberto@outlook.com>
2024-02-03 15:22:45 -0300

    Refatorando classe de MusicCommands, havia muitas repetições de variaveis iguais então criei props para gerenciar o estado das execuções, agora é só utilizar as propriedas de classe e chamar elas de qualqer lugar pois elas sempre estão no mesmo contexto

--- Commands/MusicCommands.cs
+++ Commands/MusicCommands.cs
@@ -14,46 +14,47 @@ namespace discord_bot.Commands
     public class MusicCommands : BaseCommandModule
     {
         private readonly Dictionary<ulong, Queue<LavalinkTrack>> _musicQueues = new Dictionary<ulong, Queue<LavalinkTrack>>();
+        private CommandContext _commandContext { get; set; }
+        private LavalinkExtension _lavalink => _commandContext.Client.GetLavalink();
+        private LavalinkNodeConnection _nodeConnection => _lavalink.ConnectedNodes.Values.FirstOrDefault();   
 
         [Command("play")]
         public async Task Join(CommandContext ctx, [RemainingText] string search)
         {
-            var lavalink = ctx.Client.GetLavalink();
-            var node = lavalink.ConnectedNodes.Values.FirstOrDefault();
+            _commandContext = ctx;
 
-            if (node == null)
+            if (_nodeConnection == null)
             {
                 await ctx.RespondAsync("Opa, parece que o lavalink está com problemas, o cabaço me programou mal");
             }
             else
             {
-                var channel = ctx.Member?.VoiceState?.Channel;
+                var channel = _commandContext.Member?.VoiceState?.Channel;
 
                 if (channel != null)
                 {
-                    var voiceConected = await node.ConnectAsync(channel);
+                    var voiceConected = await _nodeConnection.ConnectAsync(channel);
                 }
                 else
                 {
-                    await ctx.RespondAsync("Você precisa estar em um canal para o bot saber onde entrar, mas é muito burro mesmo");
+                    await _commandContext.RespondAsync("Você precisa estar em um canal para o bot saber onde entrar, mas é muito burro mesmo");
                     return;
                 }
             }
 
-            node = lavalink.ConnectedNodes.Values.First();
-            var conn = node.GetGuildConnection(ctx.Member.VoiceState.Guild);
+            var conn = _nodeConnection.GetGuildConnection(ctx.Member.VoiceState.Guild);
 
             if (conn == null)
             {
-                await ctx.RespondAsync("Opa, parece que o lavalink está com problemas, o cabaço me programou mal");
+                await _commandContext.RespondAsync("Opa, parece que o lavalink está com problemas, o cabaço me programou mal");
                 return;
             }
 
-            var loadResult = await node.Rest.GetTracksAsync(search);
+            var loadResult = await _nodeConnection.Rest.GetTracksAsync(search);
 
             if (loadResult.LoadResultType == LavalinkLoadResultType.LoadFailed || loadResult.LoadResultType == LavalinkLoadResultType.NoMatches)
             {
-                await ctx.RespondAsync($"Não achei nada pra essa pesquisa {search}.");
+                await _commandContext.RespondAsync($"Não achei nada pra essa pesquisa {search}.");
                 return;
             }
 
@@ -64,14 +65,14 @@ namespace discord_bot.Commands
                 var track = tracks.First();
 
                 // Adiciona a música à fila
-                EnqueueTrack(ctx.Member.VoiceState.Guild.Id, track);
+                EnqueueTrack(_commandContext.Member.VoiceState.Guild.Id, track);
 
-                await ctx.RespondAsync($"Adicionado à fila: {track.Title}");
+                await _commandContext.RespondAsync($"Adicionado à fila: {track.Title}");
 
                 // Se não estiver tocando nada, comece a tocar
-                if (!IsPlaying(ctx.Member.VoiceState.Guild.Id))
+                if (!IsPlaying(_commandContext.Member.VoiceState.Guild.Id))
                 {
-                    await PlayNextTrack(ctx, ctx.Member.VoiceState.Guild.Id);
+                    await PlayNextTrack(_commandContext.Member.VoiceState.Guild.Id);
                 }
             }
             else if (tracks.Count() > 1)
@@ -81,7 +82,7 @@ namespace discord_bot.Commands
 
                 foreach (var track in tracks)
                 {
-                    EnqueueTrack(ctx.Member.VoiceState.Guild.Id, track);
+                    EnqueueTrack(_commandContext.Member.VoiceState.Guild.Id, track);
                 }
 
                 string queueString = string.Join(", ", _musicQueues
@@ -89,12 +90,12 @@ namespace discord_bot.Commands
                                                         .SelectMany(pair => pair.Value)
                                                         .Select(track => track.Title));
 
-                await ctx.RespondAsync($"Músicas na fila: {queueString}");
+                await _commandContext.RespondAsync($"Músicas na fila: {queueString}");
 
                 // Se não estiver tocando nada, comece a tocar
-                if (!IsPlaying(ctx.Member.VoiceState.Guild.Id))
+                if (!IsPlaying(_commandContext.Member.VoiceState.Guild.Id))
                 {
-                    await PlayNextTrack(ctx, ctx.Member.VoiceState.Guild.Id);
+                    await PlayNextTrack(_commandContext.Member.VoiceState.Guild.Id);
                 }
             }
         }
@@ -102,40 +103,39 @@ namespace discord_bot.Commands
         [Command("skip")]
         public async Task Skip(CommandContext ctx)
         {
-            var lavalink = ctx.Client.GetLavalink();
-            var node = lavalink.ConnectedNodes.Values.FirstOrDefault();
+            _commandContext = ctx;
 
-            if (node == null)
+            if (_nodeConnection == null)
             {
                 // O Lavalink não está conectado, maneje isso conforme necessário
                 return;
             }
 
-            var guildId = ctx.Member.VoiceState.Guild;
+            var guild = ctx.Member.VoiceState.Guild;
 
             if (!_musicQueues.TryGetValue(ctx.Member.VoiceState.Guild.Id, out var queue) || queue.Count == 0)
             {
-                await ctx.RespondAsync("Não há músicas na fila para pular.");
+                await _commandContext.RespondAsync("Não há músicas na fila para pular.");
                 return;
             }
 
-            var conn = node.GetGuildConnection(guildId);
+            var conn = _nodeConnection.GetGuildConnection(guild);
 
             // Para a música atual
             await conn.StopAsync();
 
             // Pula para a próxima música na fila (se houver)
-            await PlayNextTrack(ctx, ctx.Member.VoiceState.Guild.Id);
+            await PlayNextTrack(ctx.Member.VoiceState.Guild.Id);
 
-            await ctx.RespondAsync("Música pulada com sucesso.");
+            await _commandContext.RespondAsync("Música pulada com sucesso.");
         }
 
 
         [Command("stop")]
         public async Task Stop(CommandContext ctx)
         {
-            var node = ctx.Client.GetLavalink().ConnectedNodes.Values.First();
-            var conn = node.GetGuildConnection(ctx.Member.VoiceState.Guild);
+            _commandContext = ctx;
+            var conn = _nodeConnection.GetGuildConnection(ctx.Member.VoiceState.Guild);
             await conn.DisconnectAsync();
         }
 
@@ -156,22 +156,21 @@ namespace discord_bot.Commands
             return !_musicQueues.TryGetValue(guildId, out var queue) && queue.Count > 0;
         }
 
-        private async Task PlayNextTrack(CommandContext ctx, ulong guildId)
+        private async Task PlayNextTrack(ulong guildId)
         {
             if (_musicQueues.TryGetValue(guildId, out var queue) && queue.Count > 0)
             {
                 var nextTrack = queue.Dequeue();
-                var node = ctx.Client.GetLavalink().ConnectedNodes.Values.First();
-                var conn = node.GetGuildConnection(ctx.Member.VoiceState.Guild);
+                var conn = _nodeConnection.GetGuildConnection(_commandContext.Member.VoiceState.Guild);
 
                 await conn.PlayAsync(nextTrack);
 
-                await ctx.RespondAsync($"Now playing {nextTrack.Title}!");
+                await _commandContext.RespondAsync($"Now playing {nextTrack.Title}!");
 
                 conn.PlaybackFinished += async (sender, args) =>
                 {
                     // Quando a música termina, toque a próxima da fila (se houver)
-                    await PlayNextTrack(ctx, guildId);
+                    await PlayNextTrack(guildId);
                 };
             }
         }
